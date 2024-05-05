94b9baa361ebc507e692d100b80d007e8bc84f8c
Hecate-vv <wdobrodziej@gmail.com>
2024-05-05 19:22:13 +0200

    zad3

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import company.models.Manager;
 import company.models.Worker;
 import company.abstracts.Employee;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,47 +11,23 @@ public class Main {
         Worker worker5 = new Worker("Wojciech", 2600, 6, "19-07-1988", "Web Developer");
         Worker worker6 = new Worker("Bogumisława", 1900, 7, "04-07-2007", "Tester");
         Worker worker7 = new Worker("Ala", 3000, 7, "04-07-2007", "Senior Tester");
-        Worker worker8 = new Worker("0la", 2000, 8, "05-10-2010", "Tester");
-        Worker worker9 = new Worker("Ela", 2000, 9, "05-10-2010", "Tester");
-        Manager manager1 = new Manager("Michał", 5489, 5, "20-02-2020", "HR Manager");
-        Manager manager2 = new Manager("Wiktoria", 6000, 7, "21-06-2021", "Project Manager");
-        Manager manager3 = new Manager("Błażej", 5500, 11, "12-12-2020", "Community Manager");
+        Manager manager = new Manager("Michał", 5489, 5, "20-02-2020", "HR Manager");
 
-        double totalSalaryAllWorkers = 0;
-        double totalSalaryAllManagers = 0;
-        double totalSalaryAllEmployees = 0;
-        ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(worker1);
-        employees.add(worker2);
-        employees.add(worker3);
-        employees.add(worker4);
-        employees.add(worker5);
-        employees.add(worker6);
-        employees.add(worker7);
-        employees.add(worker8);
-        employees.add(worker9);
-        employees.add(manager1);
-        employees.add(manager2);
-        employees.add(manager3);
+        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
+        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
+        System.out.println(worker3.getName() + " has code: " + worker3.hashCode());
+        System.out.println(worker4.getName() + " has code: " + worker4.hashCode());
+        System.out.println(worker5.getName() + " has code: " + worker4.hashCode());
+        System.out.println(worker6.getName() + " has code: " + worker4.hashCode());
+        System.out.println(worker7.getName() + " has code: " + worker4.hashCode());
+        System.out.println(manager.getName() + " has code: " + manager.hashCode());
 
-         for (Employee employee : employees) {
-            totalSalaryAllEmployees += employee.getSalary();
-            if (employee instanceof Manager) {
-                totalSalaryAllManagers += employee.getSalary();
-            }
-            if (employee instanceof Worker) {
-                totalSalaryAllWorkers += employee.getSalary();
-            }
-        }
-        System.out.println("Total salary of all workers: " + totalSalaryAllWorkers);
-        System.out.println("Total salary of all managers: " + totalSalaryAllManagers);
-        System.out.println("Total salary of all employees: " + totalSalaryAllEmployees);
-        for (Employee employee : employees) {
-            for (Employee otherEmployee : employees) {
-                if (employee != otherEmployee && employee.hashCode() == otherEmployee.hashCode()) {
-                    System.out.println("Found matching IDs: " + employee.getName() + " and " + otherEmployee.getName());
-                }
-            }
-        }
+        System.out.println(worker6.getName() + " equals worker1: " + worker6.equals(worker1));
+        System.out.println(worker6.getName() + " equals worker2: " + worker6.equals(worker2));
+        System.out.println(worker6.getName() + " equals worker3: " + worker6.equals(worker3));
+        System.out.println(worker6.getName() + " equals worker4: " + worker6.equals(worker4));
+        System.out.println(worker6.getName() + " equals worker5: " + worker6.equals(worker5));
+        System.out.println(worker6.getName() + " equals worker7: " + worker6.equals(worker7));
+        System.out.println(worker6.getName() + " equals manager: " + worker6.equals(manager));
     }
 }
