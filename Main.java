f5aa5f61c255b16594fafbf333cfa9565aa1990f
Hecate-vv <wdobrodziej@gmail.com>
2024-05-05 18:26:28 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,22 +1,27 @@
 import company.models.Manager;
 import company.models.Worker;
+import java.util.ArrayList;
+import company.abstracts.Employee;
+
 public class Main {
     public static void main(String[] args) {
-        Worker worker1 = new Worker("Natasza", 3400.0, 1);
-        Worker worker2 = new Worker("Bartolomeo", 2200.0, 2);
-        Worker worker3 = new Worker("Lila", 1945.0, 3);
-        Worker worker4 = new Worker("Mint", 2012.0, 4);
-        Manager manager = new Manager("Michał", 5489, 5);
-
-        System.out.println("Salary of worker 1: " + worker1.getSalary());
-        System.out.println("Salary of worker 2: " + worker2.getSalary());
-        System.out.println("Salary of worker 3: " + worker3.getSalary());
-        System.out.println("Salary of worker 4: " + worker4.getSalary());
-
-        worker1.work();
-        worker2.work();
-        worker3.work();
-        worker4.work();
-        manager.work();
+        Worker worker1 = new Worker("Natasza", 3400.0, 1, "02-07-1976", "IT Specialist");
+        Worker worker2 = new Worker("Bartolomeo", 2200.0, 2, "02-07-1976", "IT Specialist");
+        Worker worker3 = new Worker("Lila", 1945.0, 3, "01-04-2000", "Background Artist");
+        Worker worker4 = new Worker("Mint", 2012.0, 4, "05-05-2024", "Functional Tester");
+        Manager manager = new Manager("Michał", 5489, 5, "20-02-2020", "HR Manager");
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager);
+        for (Employee employee : employees) {
+            employee.work();
+            System.out.println("- " + employee.getName() + " (ID: " + employee.hashCode() +
+                    ", Position: " + employee.getPosition() +
+                    ", Hire date: " + employee.getHireDate() +
+                    ", Salary: " + employee.getSalary() + ")");
+        }
     }
 }
